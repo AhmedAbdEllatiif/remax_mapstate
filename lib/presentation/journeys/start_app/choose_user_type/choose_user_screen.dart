e9e41bcc110598750cd4788a1c22b827575d141a
Ahmed <ahmedmohamedaneng@gmail.com>
2022-02-17 21:38:39 +0200

    intl_phone_number_input added

--- lib/presentation/journeys/start_app/choose_user_type/choose_user_screen.dart
+++ lib/presentation/journeys/start_app/choose_user_type/choose_user_screen.dart
@@ -9,23 +9,16 @@ import 'package:remax_mapstate/presentation/journeys/start_app/choose_user_type/
 import 'package:remax_mapstate/presentation/themes/theme_color.dart';
 import 'package:remax_mapstate/presentation/themes/theme_text.dart';
 
+import 'choose_user_list_widget.dart';
 import '../../../../common/constants/sizes.dart';
 import '../../../../common/screen_utils/screen_util.dart';
-import 'choose_user_list_widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 class ChooseUserScreen extends StatelessWidget {
   const ChooseUserScreen({Key? key}) : super(key: key);
 
-  void _saveUser() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool result = await prefs.setString('user', "client");
-    print("IsSaved: $result");
-  }
-
   @override
   Widget build(BuildContext context) {
-    _saveUser();
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
