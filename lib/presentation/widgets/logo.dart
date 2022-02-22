b448c33ba32e62ac18f75c7a565428d134bc2cad
Ahmed <ahmedmohamedaneng@gmail.com>
2022-02-22 15:37:45 +0200

    Logo added to drawer

--- lib/presentation/widgets/logo.dart
+++ lib/presentation/widgets/logo.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../common/constants/assets_constants.dart';
+
 class Logo extends StatelessWidget {
 
   final double height;
@@ -8,6 +10,9 @@ class Logo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Image.asset(
+      AssetsConstants.whiteLogo,
+      fit: BoxFit.cover,
+    );
   }
 }
